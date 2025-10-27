513487d5f6e9bd4a5bd375844471b2fab8e7ad35
Dmitri Melnikov <dmitri@cloudflare.com>
2025-10-27 15:28:48 +0800

    Update lsp-zero branch and config

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -3,20 +3,14 @@ return {
     'folke/neodev.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local neodev_status_ok, neodev = pcall(require, 'neodev')
-
-      if not neodev_status_ok then
-        return
-      end
-
-      neodev.setup()
+      require('neodev').setup()
     end
   },
   {
     'VonHeikemen/lsp-zero.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     cmd = 'Mason',
-    branch = 'v2.x',
+    branch = 'v4.x',
     dependencies = {
       { 'neovim/nvim-lspconfig' },
       {
@@ -25,34 +19,70 @@ return {
           pcall(vim.cmd, 'MasonUpdate')
         end
       },
-      { 'williamboman/mason-lspconfig.nvim', },
+      { 'williamboman/mason-lspconfig.nvim' },
       { 'hrsh7th/nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'L3MON4D3/LuaSnip' },
       { 'SmiteshP/nvim-navic' }
     },
     config = function()
-      local lsp = require('lsp-zero').preset({})
-
+      local lsp_zero = require('lsp-zero')
       local navic = require('nvim-navic')
 
-      lsp.on_attach(function(client, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
+      -- lsp_attach is where you enable features that only work
+      -- if there is a language server active in the file
+      local lsp_attach = function(client, bufnr)
+        local opts = {buffer = bufnr}
+
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
         if client.server_capabilities.documentSymbolProvider then
           navic.attach(client, bufnr)
         end
-      end)
+      end
 
-      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-      lsp.ensure_installed({
-        'pyright',
-        'lua_ls',
-        'gopls',
-        'rust_analyzer'
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
       })
 
-      lsp.setup()
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'pyright',
+          'lua_ls',
+          'gopls',
+          'rust_analyzer'
+        },
+        handlers = {
+          -- default handler for all servers
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+          -- custom handler for lua_ls
+          lua_ls = function()
+            require('lspconfig').lua_ls.setup({
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = {'vim'}
+                  }
+                }
+              }
+            })
+          end,
+        }
+      })
     end
   },
 }
